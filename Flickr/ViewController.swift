1627ce267deac2465941821eee10b422ab5cb242
Erdi Tunçalp <erdi.tuncalp@kafein.com.tr>
2019-03-25 14:27:44 +0300

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Erdi Tunçalp on 25.03.2019.
+//  Copyright © 2019 Erdi Tunçalp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
